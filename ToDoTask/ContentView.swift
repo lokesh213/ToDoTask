3fa3d85d42431a1f333b77a137c31dfecd1047de
Lokesh <lokesh.babu@windstream.com>
2025-05-04 15:11:57 +0530

    Initial Commit

---
+++ ToDoTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoTask
+//
+//  Created by Lokesh on 04/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoTask/ToDoTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTaskApp.swift
+//  ToDoTask
+//
+//  Created by Lokesh on 04/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
